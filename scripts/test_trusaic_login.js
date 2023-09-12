bc70893ba355df5626d182caf06068741b03ac5b
Surabhi Rajan <surabhirajan@hotmail.com>
2023-09-13 00:39:26 +0530

    Browser test script

--- scripts/test_trusaic_login.js
+++ scripts/test_trusaic_login.js
@@ -1,47 +1,57 @@
 import { browser } from "k6/experimental/browser";
-import { check } from "k6";
+import { check, sleep } from "k6";
+import http from "k6/http";
 
 export const options = {
+  insecureSkipTLSVerify: true,
   scenarios: {
-    loginui: {
+    kTestLogin: {
       executor: "constant-vus",
-      exec: "loginpageTest",
-      vus: 2,
-      duration: "10s",
+      exec: "kTestLogin",
+      vus: 1,
+      duration: "45s",
       options: {
         browser: {
           type: "chromium",
         },
       },
     },
-  },
-  thresholds: {
-    http_req_duration: ["p(95)<250"],
+    /*portal: {
+      executor: "constant-vus",
+      exec: "portalAvailability",
+      vus: 1,
+      duration: "5s",
+    },
+    loginTest: {
+      executor: "constant-vus",
+      exec: "loginTest",
+      vus: 1,
+      duration: "60s",
+      options: {
+        browser: {
+          type: "chromium",
+        },
+      },
+    },*/
   },
 };
 
-export const testconfig = {
-  loginurl: "https://portal-qa.trusaic.com",
-  username: "Secondary@mailinator.com",
-  password: "Trusaic@12",
-  passcode: "",
-};
-
-export async function loginpageTest() {
-  const browser = chromium.launch({
-    args: ["no-sandbox"],
-    headless: true,
-    timeout: "60s", // Or whatever time you want to define
+export function portalAvailability() {
+  const res = http.get("https://portal-qa.trusaic.com");
+  check(res, {
+    "status is 200": (r) => r.status === 200,
   });
+}
 
+export async function loginTest() {
   const page = browser.newPage();
-
   try {
-    await page.goto(testconfig.loginurl);
-    page.screenshot({ path: "/prometheus/screenshot.png" });
-    /*
-    page.locator('input[name="Email"]').type(testconfig.username);
-    page.locator('input[name="Password"]').type(testconfig.password);
+    await page.goto("https://portal-qa.trusaic.com");
+
+    const messagesLink = page.waitForSelector('p[class="login-info-text"]');
+    console.log(messagesLink.textContent());
+    page.locator('input[name="Email"]').type("Secondary@mailinator.com");
+    page.waitForSelector('input[name="Password"]').type("Trusaic@12");
     const submitButton = page.locator('input[id="trusaic-loginBtn"]');
     await Promise.all([page.waitForNavigation(), submitButton.click()]);
     check(page, {
@@ -49,7 +59,21 @@ export async function loginpageTest() {
         p.locator('p[class="enter-code-title"]').textContent() ==
         "Enter the code sent to",
     });
-    */
+  } finally {
+    page.close();
+  }
+}
+
+export async function kTestLogin() {
+  const page = browser.newPage();
+  try {
+    await page.goto("https://test.k6.io/my_messages.php");
+    sleep(30);
+    page.screenshot({ path: "/prometheus/01_homepage.png" });
+    // Enter login credentials
+    page.waitForSelector('input[name="login"]').type("admin");
+    page.waitForSelector('input[name="password"]').type("123");
+    page.screenshot({ path: "/prometheus/01_homepage2.png" });
   } finally {
     page.close();
   }
