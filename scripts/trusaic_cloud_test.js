e82d7847943524ef6fd006c7cb430c88a50c6bf7
Surabhi Rajan <surabhirajan@hotmail.com>
2023-09-30 03:25:40 +0530

    .

--- scripts/trusaic_cloud_test.js
+++ scripts/trusaic_cloud_test.js
@@ -1,6 +1,8 @@
 import { check, sleep } from "k6";
 import http from "k6/http";
+import { browser } from "k6/experimental/browser";
 import { appConstants } from "./general/constants.js";
+import { getMfaCode } from "./general/getMfaCode.js";
 
 const CONSTS = appConstants;
 
@@ -29,9 +31,9 @@ export const options = {
       executor: "ramping-vus",
       gracefulStop: "30s",
       stages: [
-        { target: 5, duration: "20s" },
-        { target: 10, duration: "30s" },
-        { target: 0, duration: "10m" },
+        { target: 3, duration: "20s" },
+        { target: 6, duration: "30s" },
+        { target: 0, duration: "10s" },
       ],
       gracefulRampDown: "30s",
       exec: "availabilityTest",
@@ -42,10 +44,22 @@ export const options = {
       stages: [
         { target: 2, duration: "5s" },
         { target: 5, duration: "10s" },
-        { target: 0, duration: "3m" },
+        { target: 0, duration: "3s" },
       ],
       gracefulRampDown: "30s",
       exec: "apiCallTest",
+    },
+    portal_login: {
+      executor: "per-vu-iterations",
+      options: {
+        browser: {
+          type: "chromium",
+        },
+      },
+      exec: "portalLogin",
+      startTime: "1s",
+      vus: 1,
+      iterations: 1,
     },
   },
 };
@@ -68,4 +82,41 @@ export function apiCallTest() {
   const headers = { "Content-Type": "application/json" };
   http.post("https://httpbin.test.k6.io/post", payload, { headers });
   sleep(1);
+}
+
+export async function portalLogin() {
+  const page = browser.newPage();
+  try {
+    // --- Login page
+    await page.goto(CONSTS.portalUrl);
+    page.locator('input[name="Email"]').type(CONSTS.portalUser);
+    page.locator('input[name="Password"]').type(CONSTS.portalPass);
+    const submitButton = page.locator('button[id="trusaic-loginBtn"]');
+    await Promise.all([page.waitForNavigation(), submitButton.click()]);
+    /// Add delay before reading the code.
+    sleep(3); // ms
+
+    let mfaCode = getMfaCode(
+      CONSTS.domain,
+      CONSTS.inbox,
+      CONSTS.mailUrl,
+      CONSTS.apiToken
+    );
+    console.log(mfaCode);
+    ///
+    page.locator('input[name="Code"]').type(mfaCode);
+    const mfaButton = page.locator('button[type="submit"]');
+    ///
+    await Promise.all([page.waitForNavigation(), mfaButton.click()]);
+    await page.waitForNavigation();
+    check(page, {
+      header: (p) =>
+        p.locator('span[class="Upload-Title"]').textContent() == "PayParity",
+    });
+    //
+  } catch (e) {
+    console.log("Error : portalLogin : " + e);
+  } finally {
+    page.close();
+  }
 }
