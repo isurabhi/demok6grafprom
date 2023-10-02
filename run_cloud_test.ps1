0850c950454069aa4fe262c54992f1df1b060468
Surabhi Rajan <surabhirajan@hotmail.com>
2023-10-02 22:15:01 +0530

    Update run_cloud_test.ps1
    local to cloud

--- run_cloud_test.ps1
+++ run_cloud_test.ps1
@@ -5,8 +5,12 @@
 #docker-compose run --rm k6 run -o experimental-prometheus-rw /scripts/start_trusaic_load.js 
 #docker-compose run --rm k6 run -o experimental-prometheus-rw /scripts/trusaic_cloud_test.js
 #-e K6_PROMETHEUS_RW_TREND_AS_NATIVE_HISTOGRAM=true `
-cat .\scripts\trusaic_cloud_test.js `
-| docker run --rm -i `
-    -e K6_OUT=xk6-prometheus-rw --cap-add=SYS_ADMIN `
+cat .\scripts\trusaic_cloud_test.js | `
+    docker run --rm -i --cap-add=SYS_ADMIN `
+    -e K6_OUT=xk6-prometheus-rw `
+    -e K6_PROMETHEUS_RW_INSECURE_SKIP_TLS_VERIFY=true `
+    -e K6_PROMETHEUS_RW_PUSH_INTERVAL=5s `
     -e K6_PROMETHEUS_RW_SERVER_URL=https://prometheus-prod-13-prod-us-east-0.grafana.net/api/prom/push `
+    -e K6_PROMETHEUS_RW_USERNAME=1212056 `
+    -e K6_PROMETHEUS_RW_PASSWORD=glc_eyJvIjoiOTU1MzE1IiwibiI6InBkYy1xYXRydXNhaWMtZGVmYXVsdC1xYXRydXNhaWNrNi13cmlldCIsImsiOiJkejc2SDdJM2dFOVkzMDVKWE5uMWI4ZEciLCJtIjp7InIiOiJwcm9kLXVzLWVhc3QtMCJ9fQ== `
     grafana/k6:master-with-browser run -o experimental-prometheus-rw -
